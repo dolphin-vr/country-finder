49b0fbf8d27d322f138e9585c34cb3497dbef5e0
yurchykyur <yurchyk.yur.s@gmail.com>
2023-09-23 12:36:26 +0300

    finish task-02

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,11 +1,54 @@
-import { Section, Container, CountryInfo, Loader } from 'components';
+import { Section, Container, CountryInfo, Loader, Heading } from 'components';
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const [countryInfo, setCountryInfo] = useState([]);
+  const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const { countryId } = useParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    setLoader(true);
+    async function getData() {
+      try {
+        const responce = await fetchCountry(countryId);
+        console.log(responce);
+        setCountryInfo(responce);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoader(false);
+      }
+    }
+    getData();
+  }, [countryId]);
+
+  const { flag, capital, country, id, languages, population } = countryInfo;
+
+  const backLink = location.state?.from ?? '/';
+
   return (
-    <Section>
-      <Container>
-        <h2>Country</h2>
-      </Container>
-    </Section>
+    <>
+      <Link to={backLink}>
+        <button style={{ color: 'white' }}> --- Go back</button>
+      </Link>
+      {loader && <Loader />}
+      <Section>
+        <Container>
+          <CountryInfo
+            flag={flag}
+            capital={capital}
+            country={country}
+            id={id}
+            languages={languages}
+            population={population}
+          />
+        </Container>
+      </Section>
+      {error && <Heading>{error}</Heading>}
+    </>
   );
 };
